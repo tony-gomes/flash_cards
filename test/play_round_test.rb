b277a2cbf302f3a9f600bb5d777d3207cac05f57
Anthony Gomes <code@tonygom.es>
2020-02-02 13:48:25 -0700

    Wrapped processes in PlayRound in methods & added tests for PlayRound

--- test/play_round_test.rb
+++ test/play_round_test.rb
@@ -11,7 +11,13 @@ class PlayRoundTest < MiniTest::Test
     @new_game = PlayRound.new
   end
 
-  def test_play_round_starts
+  def test_play_round_exists
     assert_instance_of PlayRound, @new_game
+  end
+
+  def test_play_round_has_deck_with_cards
+    assert_instance_of Deck, @new_game.deck
+    assert_instance_of Card, @new_game.deck.cards.first
+    assert_instance_of Card, @new_game.deck.cards.last
   end
 end
