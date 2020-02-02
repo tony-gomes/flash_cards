dd97bf9c667d551dbacdd4f1c821445fcb7c691e
Anthony Gomes <code@tonygom.es>
2020-02-01 22:47:35 -0700

    Add Ruby flashcards file, and modified PlayRound to incorporate the additional deck.

--- lib/play_round.rb
+++ lib/play_round.rb
@@ -4,7 +4,26 @@ require './lib/deck'
 require './lib/round'
 
 class PlayRound
-  data = 'cards.txt'
+
+  puts "\n\nChoose Your Fate\n"
+  puts "-------------------------------------------------\n\n"
+  puts "\n\nChoose A Deck\n"
+  puts "Ruby or Trivia"
+
+  card_path = gets.chomp.to_s
+  # until card_path.downcase == "ruby" || card_path.downcase == "trivia"
+  #   card_path
+  # end
+
+  if card_path.downcase == "trivia"
+    data = 'trivia_cards.txt'
+  elsif card_path.downcase == "ruby"
+    puts "Hello Rubyist! Please identify the following methods using this syntax\n"
+    puts ".method or .method_name\n"
+    puts "Good Luck. You Belong Here!\n"
+    data = 'ruby_cards.txt'
+  end
+
   cards = CardGenerator.new(data).cards
   @deck = Deck.new(cards)
   @round = Round.new(@deck)
@@ -14,7 +33,9 @@ class PlayRound
 
   until @round.flip >= @deck.cards.length
     puts "This is card number #{@round.flip + 1} out of #{@deck.cards.length}"
+    puts "Category: " + @round.current_card.category
     puts "Question: " + @round.current_card.question
+
     guess = gets.chomp.to_s
     take_turn = @round.take_turn(guess)
     puts "\n" + take_turn.feedback + "\n\n"
